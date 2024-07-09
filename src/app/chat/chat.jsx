53b702fdafd3d374df92b545683a5787440b9b24
Dev Sharma <devsharmaelc@gmail.com>
2024-07-09 16:24:18 +0530

    add reward

--- src/app/chat/chat.jsx
+++ src/app/chat/chat.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import CommentIcon from "@mui/icons-material/Comment";
 const Chat = () => {
   return (
-    <div className="fixed bottom-5 right-5 text-white z-30 ">
+    <div className="fixed md:bottom-5 bottom-20 right-5 text-white z-30 ">
       <CommentIcon className="h-10 w-10 text-black bg-gray-100 cursor-pointer rounded-full p-2 hover:h-12 hover:w-12 transition-all duration-300" />
     </div>
   );
